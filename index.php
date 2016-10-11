9c35c88177fde10c8ded034deae46e8e34c7a82a
markel95 <markelarra95@gmail.com>
2016-10-11 07:30:54 +0000

    lelengo komit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+require 'vendor/autoload.php';
+$Essence = new Essence\Essence();
+$Media = $Essence->extract('https://www.youtube.com/watch?v=XA0Mcb8cWd0');
+?>
+
+<article>
+    <header>
+        <h1><?php echo $Media->title; ?></h1>
+        <p>By <?php echo $Media->authorName; ?></p>
+    </header>
+
+    <div class="player">
+        <?php echo $Media->html; ?>
+    </div>
+</article>
+
+</html>
